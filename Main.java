88b6dd45449d64dc8b226a1b0f3bafe9ab6d3319
joshuacassidy <Joshcassidy79@gmail.com>
2017-08-27 13:33:51 +0100

    Cleaned up code to be less repetitive

--- Main.java
+++ Main.java
@@ -1,37 +1,32 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main  {
 
-
-
     public static void main(String[] args) {
-        long numberOfTerms = 0;
-
-        try( Scanner in = new Scanner( System.in )) {
+        long numberOfTerms;
+        try( Scanner scanner = new Scanner( System.in )) {
             System.out.println("How many terms would you like of each series to be printed?");
-            numberOfTerms = in.nextLong();
-
-            System.out.println("The first " + numberOfTerms + " terms of the Arithmetic Series is: ");
-            for(int i = 0; i < numberOfTerms; i++){
-                System.out.println("The Sum of the first " + i +" numbers: " + Series.nSum(i));
-            }
-
-            System.out.println("The first " + numberOfTerms + " terms of the Factorial Series is: ");
-
-            for(int i = 0; i < numberOfTerms; i++){
-                System.out.println(i +" factorial: " + Series.factorial(i));
-            }
-
-            System.out.println("The first " + numberOfTerms + " terms of the Fibonacci Series is: ");
-            for(int i = 0; i < numberOfTerms; i++){
-                System.out.println("The " + i +" number of the fibonacci sequence: " + Series.fibonacci(i));
-            }
-
+            numberOfTerms = scanner.nextLong();
+            compute(numberOfTerms);
         } catch (InputMismatchException e){
             System.out.println("Invalid input");
         }
 
-
     }
+
+    public static void compute(long numberOfTerms) {
+        String seriesType[] = {"Arithmetic", "Factorial", "Fibonacci"};
+        for (String i: seriesType) {
+            System.out.println("The first " + numberOfTerms + " terms of the " + i + " Series is: ");
+            for (int j = 0; j < numberOfTerms; j++) {
+                System.out.println("The Sum of the first " + j + " numbers: " + series(j,Arrays.asList(seriesType).indexOf(i)));
+            }
+        }
+    }
+
+    public static long series(int func,int choice){
+        Long listOfSeries[] = { Series.nSum(func), Series.factorial(func), Series.fibonacci(func)};
+        return listOfSeries[choice];
+    }
+
 }
